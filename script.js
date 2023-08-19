04791e9715ef5e280b9196ec2a36bc4fd16ce627
gracehjy <g_huang21@yahoo.com>
2023-08-19 18:25:16 -0500

    created a working ui

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-//create variables for user input and comp moves
-let userMove = prompt("Pick a move: ").toLowerCase();
-let moves = ["rock", "paper", "scissor"];
 
-//create a function that randomly generates a move
-function randomMove() {
-    return moves[parseInt((Math.random()*3))];
-}
-
-let compMove = randomMove();
-console.log("Computer's move: "+compMove);
-
-//create a function that checks for a win
+//check for a win and update scores
 function checkForWin(userMove, compMove){
+    document.getElementById('compMove').innerHTML = "Computer chose: "+compMove;
     if(userMove === "rock" && compMove === "scissor"
     || userMove === "scissor" && compMove === "paper"
     || userMove === "paper" && compMove === "rock"){
-        console.log("You win!");
-        return true;
+        document.getElementById('result').innerHTML = "Result: Congrats, You Won!";
     }
     else if(compMove === "rock" && userMove === "scissor"
     || compMove === "scissor" && userMove === "paper"
     || compMove === "paper" && userMove === "rock"){
-        console.log("You lost! Better luck next time...");
-        return true;
+        compScore++;
+        document.getElementById('result').innerHTML = "Result: Sorry, You Lost...";
     }
     else{
-        console.log("Tie!");
-        return false;
+        document.getElementById('result').innerHTML = "Result: Tie!";
     }
 }
 
-//while the there hasn't been a win, randomly generate a move and ask the user for input
-let win = checkForWin(userMove, compMove);
-while(win === false){
-    userMove = prompt("Pick a move: ").toLowerCase();
-    compMove = randomMove();
-    console.log("Computer's move: "+compMove);
-    win = checkForWin(userMove, compMove);
+//get compMove
+let moves = ['rock', 'paper', 'scissor'];
+function randomMove() {
+    return moves[parseInt((Math.random()*3))];
+}
+
+
+//now we make it all actually work lmao
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissor = document.getElementById('scissor');
+
+rock.addEventListener('click', () => game('rock'));
+paper.addEventListener('click', () => game('paper'));
+scissor.addEventListener('click', () => game('scissor'));
+
+function game(userMove){
+    const compMove = randomMove();
+    checkForWin(userMove, compMove);
 }
